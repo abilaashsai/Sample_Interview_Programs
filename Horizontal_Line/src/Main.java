fa04723f8f2cf6bf9b1f5feeacb85522b358c3c9
abilashr <abilashr@INabilashr.local>
2016-05-17 12:55:00 +0530

    Getting input n from the user

--- Horizontal_Line/src/Main.java
+++ Horizontal_Line/src/Main.java
@@ -1,7 +1,28 @@
+import com.sun.javafx.css.ParsedValueImpl;
+import com.sun.tools.internal.ws.wsdl.document.jaxws.Exception;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) {
-        for(int i=0;i<8;i++)
-            System.out.print("*");
-    }
+        System.out.println("Enter number n");
+        System.out.println();
+        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+        int input;
+        try
+        {
+            input = Integer.parseInt(reader.readLine());
+            for (int i = 0; i < 8; i++) {
+                System.out.print("*");
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println(e);
+
+        }
+        }
 }
