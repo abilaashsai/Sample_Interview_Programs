5d615a2d80546516bcf1406e8f02b78f055b8e1e
abilashr <abilashr@INabilashr.local>
2016-05-17 16:49:35 +0530

    FizzBuzz

---
+++ FizzBuzz/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+public void fizzbuzz(int i)
+{
+    if((i%3)==0 && (i%5)==0)
+    {
+        System.out.println("FizzBuzz");
+
+    }
+    else if(i%3==0)
+    {
+        System.out.println("Fizz");
+
+    }
+    else if(i%5==0)
+    {
+        System.out.println("Buzz");
+
+    }
+    else
+    {
+        System.out.println(i);
+
+
+    }
+}
+    public static void main(String[] args) {
+        Main main=new Main();
+for(int i=1;i<=100;i++){
+    main.fizzbuzz(i);
+
+}
+    }
+}
