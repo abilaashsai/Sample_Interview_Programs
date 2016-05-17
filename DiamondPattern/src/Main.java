ae2d6a3500f2f021512f7e3bf298812a89fcdd1b
abilashr <abilashr@INabilashr.local>
2016-05-17 16:17:31 +0530

    Diamond Pattern

---
+++ DiamondPattern/src/Main.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Enter number n");
+        System.out.println();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int input;
+        try {
+            input = Integer.parseInt(reader.readLine());
+            for (int i = 1; i <= 2*input-1; i++) {
+                for (int j = 1; j <=input- Math.abs((input)-Math.abs(input-i)); j++)
+                {
+                    System.out.print(" ");
+                }
+               for (int j = 1; j <= (2*Math.abs(Math.abs(input - i)-(input) )) - 1; j++) {
+                    System.out.print("*");
+               }
+                System.out.println();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
